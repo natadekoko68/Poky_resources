a21823f8aafaa6eb6f6b7f9175219c140992854b
Kotaro Hara <125146909+natadekoko68@users.noreply.github.com>
2024-12-10 16:25:01 +0900

    Update list_to_csv.py

--- list_to_csv.py
+++ list_to_csv.py
@@ -12,8 +12,9 @@ def get_num(x):
     else:
         return int(ret[0])
 
-def list_to_csv(input_file, output_file = input_file.replace(".list", ".csv")):
-
+def list_to_csv(input_file, output_file = None):
+    if output_file == None:
+        input_file.replace(".list", ".csv")
     pattern = r"^\s*(\S+)\s+([\d.]+)\s+([\d.]+)\s+([\d]+)\s+([\d]+)"
 
     rows = []
